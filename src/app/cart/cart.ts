8219c0727c9ee5db7730078e6ef52ca592d41dbf
phanna123 <soksiphanna35@gmail.com>
2026-03-14 20:28:34 +0700

    update final

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -2,11 +2,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../services/api-service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule, TranslateModule], 
   templateUrl: './cart.html'
 })
 export class Cart  {
